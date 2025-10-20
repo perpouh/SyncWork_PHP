80bc006c3223ab279bca17eb345241dbe3ebd6a7
perpouh <perpouh@gmail.com>
2025-10-20 10:14:05 +0900

    feat: チケット詳細画面 ひどいもんだがまあ細かいことはあとでいいか

--- containers/laravel/resources/js/pages/tickets/Show.vue
+++ containers/laravel/resources/js/pages/tickets/Show.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>
+  <AppLayout :breadcrumbs="breadcrumbItems">
+    <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
     <h1>{{ ticket.title }}</h1>
+    <div>
+      {{ ticket.description }}
+    </div>
+    <div>
+      <span>{{ ticket.status }}</span>
+      <span>{{ ticket.priority }}</span>
+      <span>{{ ticket.assignee }}</span>
+      <span>{{ ticket.type }}</span>
+      <span>{{ ticket.created_at }}</span>
+      <span>{{ ticket.updated_at }}</span>
+    </div>
   </div>
+  </AppLayout>
 </template>
 
 <script setup lang="ts">
 import type { Ticket, Project } from '@/types';
+import { computed } from 'vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
 
-defineProps<{
+const props = defineProps<{
+  project: Project,
   ticket: Ticket;
-  project: Project;
 }>();
+
+const project = computed(() => props.project);
+
+const breadcrumbItems: BreadcrumbItem[] = [
+  { title: project.value.title, href: "projects/" + project.value.id },
+];
 </script>
